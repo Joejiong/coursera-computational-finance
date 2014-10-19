716490af54949a4ecff04b810057cd67138c455a
vaclavcadek@gmail.com <vaclavcadek@gmail.com>
2014-10-19 14:32:41 +0200

    first part of HW3 almost done - need to write csv values, and perform portfolio analyze

--- week5/marketsim.py
+++ week5/marketsim.py
@@ -4,9 +4,9 @@ import datetime as dt
 import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.DataAccess as da
 import copy
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
-starting_cash = sys.argv[1]
+starting_cash = float(sys.argv[1])
 orders_csv = sys.argv[2]
 values_csv = sys.argv[3]
 dates = []
@@ -26,21 +26,26 @@ df_prices = dataobj.get_data(timestamps, symbols, 'close')
 trade_matrix = copy.deepcopy(df_prices)
 trade_matrix = trade_matrix * 0
 
+ts_cash = Series(len(timestamps)*0, index=timestamps)
+ts_cash[0] = starting_cash
+trade_matrix['_CASH'] = ts_cash
 for row in csv.reader(open(orders_csv, 'rU'), delimiter=','):
     date = dt.datetime(int(row[0]), int(row[1]), int(row[2]), hour=16)
     symbol = row[3]
     operation = row[4]
-    amount = row[5]
+    amount = float(row[5])
     if operation == 'Buy':
-       trade_matrix[symbol].ix[date] += float(amount)
+       trade_matrix[symbol].ix[date] += amount
+       trade_matrix['_CASH'].ix[date] -= amount * df_prices[symbol].ix[date]
     else:
-       trade_matrix[symbol].ix[date] -= float(amount)
-
+       trade_matrix[symbol].ix[date] -= amount
+       trade_matrix['_CASH'].ix[date] += amount * df_prices[symbol].ix[date]
 df_holdings = copy.deepcopy(trade_matrix.cumsum())
+df_prices['_CASH'] = 1.0
 
-# FIXME: Use dot product for value of whole portfolio :)
-print DataFrame(df_prices*df_holdings)
-
+ts_total = DataFrame(df_prices*df_holdings).sum(axis=1)
+df_holdings['_VALUE'] = ts_total
+print df_holdings
 
 
 
