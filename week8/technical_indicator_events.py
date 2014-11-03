8edf4ffc3a679fdde94a9d4462c70b214b72702d
vaclavcadek@gmail.com <vaclavcadek@gmail.com>
2014-11-03 17:28:43 +0100

    hw6 elaborated - need to fix the key error

---
+++ week8/technical_indicator_events.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import math
+import copy
+import QSTK.qstkutil.qsdateutil as du
+import datetime as dt
+import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkstudy.EventProfiler as ep
+import sys
+from pandas import rolling_mean, rolling_std, DataFrame
+
+
+def compute_bollinger_bands(prices, look_back):
+    bollinger_vals = DataFrame.copy(prices)
+    for symbol in bollinger_vals._series:
+        rolling_mean_vals = rolling_mean(prices[symbol], look_back, min_periods=look_back)
+        rolling_std_vals = rolling_std(prices[symbol], look_back, min_periods=look_back)
+        bollinger_vals[symbol] = (prices[symbol] - rolling_mean_vals) / rolling_std_vals
+    return bollinger_vals
+
+
+def find_events(bollinger_vals):
+    ts_market = bollinger_vals['SPY']
+
+    print "Finding Events"
+
+    df_events = copy.deepcopy(bollinger_vals)
+    df_events = df_events * np.NAN
+
+    # Time stamps for the event range
+    ldt_timestamps = bollinger_vals.index
+
+    for i in range(1, len(ldt_timestamps)):
+        for symbol in bollinger_vals._series:
+            # Calculating the returns for this timestamp
+            bollinger_equity_today = bollinger_vals[symbol].ix[ldt_timestamps[i]]
+            bollinger_equity_yesterday = bollinger_vals[symbol].ix[ldt_timestamps[i - 1]]
+            bollinger_market_today = ts_market.ix[ldt_timestamps[i]]
+
+            if bollinger_equity_today <= -2 and bollinger_equity_yesterday >= -2 and bollinger_market_today >= 1:
+                df_events[symbol].ix[ldt_timestamps[i]] = 1
+    return df_events
+
+
+dt_start = dt.datetime(2008, 1, 1)
+dt_end = dt.datetime(2009, 12, 31)
+ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+
+dataobj = da.DataAccess('Yahoo')
+symbols = dataobj.get_symbols_from_list('sp5002012')
+symbols.append('SPY')
+
+stock_prices = dataobj.get_data(ldt_timestamps, symbols, 'close')
+stock_prices = stock_prices.fillna(method='ffill')
+stock_prices = stock_prices.fillna(method='bfill')
+stock_prices = stock_prices.fillna(1.0)
+
+bollinger_vals = compute_bollinger_bands(stock_prices, 20)
+print bollinger_vals
+events = find_events(bollinger_vals)
+print events
+print "Creating Study"
+ep.eventprofiler(events, stock_prices, i_lookback=20, i_lookforward=20,
+                 s_filename='MyEventStudy.pdf', b_market_neutral=True, b_errorbars=True,
+                 s_market_sym='SPY')
