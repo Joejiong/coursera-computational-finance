14aea944c21aef9aa0dd67970a250ed1f6f889e2
vaclavcadek@gmail.com <vaclavcadek@gmail.com>
2014-11-03 15:02:56 +0100

    HW5 slightly rewrited to be possible compute bollinger vals for whole data frame - including multiple symbols

--- week7/technical_indicators.py
+++ week7/technical_indicators.py
@@ -2,36 +2,43 @@ import QSTK.qstkutil.qsdateutil as du
 import datetime as dt
 import QSTK.qstkutil.DataAccess as da
 import matplotlib.pyplot as plt
-from pandas import rolling_mean, rolling_std
+from pandas import rolling_mean, rolling_std, DataFrame
 
-symbol = 'MSFT'
+symbols = ['AAPL', 'GOOG', 'IBM', 'MSFT']
 start_date = dt.datetime(2010, 1, 1)
 end_date = dt.datetime(2010, 12, 31)
 look_back = 20
 
 
+def plot_bollinger_band(prices, symbol):
+    plt.clf()
+    rolling_mean_vals = rolling_mean(prices[symbol], look_back, min_periods=look_back)
+    rolling_std_vals = rolling_std(prices[symbol], look_back, min_periods=look_back)
+    lower_bond_vals = rolling_mean_vals - rolling_std_vals
+    upper_bond_vals = rolling_mean_vals + rolling_std_vals
+    plt.plot(prices.index, prices[symbol].values, label=symbol)
+    plt.plot(prices.index, rolling_mean_vals)
+    plt.plot(prices.index, lower_bond_vals)
+    plt.plot(prices.index, upper_bond_vals)
+    plt.legend([symbol, 'Moving Avg.', 'Lower Bond', 'Upper Bond'])
+    plt.ylabel('Adjusted Close')
+    plt.savefig("movingavg-ex.png", format='png')
+
+
 def compute_bollinger_bands(prices):
-    prices['ROLLING_MEAN'] = rolling_mean(prices[symbol], look_back, min_periods=look_back)
-    prices['ROLLING_STD'] = rolling_std(prices[symbol], look_back, min_periods=look_back)
-    prices['UPPER_BOND'] = prices['ROLLING_MEAN'] + prices['ROLLING_STD']
-    prices['LOWER_BOND'] = prices['ROLLING_MEAN'] - prices['ROLLING_STD']
-    prices['BOLLINGER_VAL'] = (prices[symbol] - prices['ROLLING_MEAN']) / prices['ROLLING_STD']
-    return prices
+    bollinger_vals = DataFrame.copy(prices)
+    for symbol in symbols:
+        rolling_mean_vals = rolling_mean(prices[symbol], look_back, min_periods=look_back)
+        rolling_std_vals = rolling_std(prices[symbol], look_back, min_periods=look_back)
+        bollinger_vals[symbol] = (prices[symbol] - rolling_mean_vals) / rolling_std_vals
+    return bollinger_vals
+
 
 timestamps = du.getNYSEdays(start_date, end_date, dt.timedelta(hours=16))
 dataobj = da.DataAccess('Yahoo')
-stock_prices = dataobj.get_data(timestamps, [symbol], 'close')
-prices = compute_bollinger_bands(stock_prices)
-plt.clf()
-plt.plot(prices.index, prices[symbol].values, label=symbol)
-plt.plot(prices.index, prices['ROLLING_MEAN'].values)
-plt.plot(prices.index, prices['UPPER_BOND'].values)
-plt.plot(prices.index, prices['LOWER_BOND'].values)
-plt.legend([symbol, 'Moving Avg.', 'Upper Bond', 'Lower Bond'])
-plt.ylabel('Adjusted Close')
-plt.savefig("movingavg-ex.png", format='png')
+stock_prices = dataobj.get_data(timestamps, symbols, 'close')
+bollinger_vals = compute_bollinger_bands(stock_prices)
 
-print prices['BOLLINGER_VAL'].ix[dt.datetime(2010, 5, 12, 16)]
 
 
 
